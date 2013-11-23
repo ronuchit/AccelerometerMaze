a3b778d18e9e35b159ab33712caff5d0ef134e9a
Rohan <ronuchit@yahoo.com>
2013-11-23 13:11:36 -0800

    adding useful links

--- AccelListener.py
+++ AccelListener.py
@@ -1,17 +1,20 @@
-DATA_PER_INTERVAL = 5;
+# DATA_PER_INTERVAL = 5;
 INTERVAL = 0.1;
 
 class AccelListener(object):
   up, down, left, right = 0, 1, 2, 3
   
   def __init__(self):
-    self.prev_avg = 0.0;
+    self.prev_samples = [];
+    self.curr_data = []
     self.change_threshold = 1.0;
     self.going_back = False
 
   def refresh(self):
     with open("data/accel_data.txt", "r+") as f:
-      curr_data = f.readlines()[-DATA_PER_INTERVAL:]
+      self.curr_data = f.readlines()
 
   def listen(self):
+    while True:
+      
     
