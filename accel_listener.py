42764a268eaae97366c1b2722b9c115ec233aeb0
Rohan <ronuchit@yahoo.com>
2013-11-23 15:39:18 -0800

    fixes for accel_listener

--- accel_listener.py
+++ accel_listener.py
@@ -2,16 +2,13 @@ import time
 
 # DATA_PER_INTERVAL = 5;
 INTERVAL = 0.1;
+UP, DOWN, LEFT, RIGHT = 0, 1, 2, 3
 
-class AccelListener(object):
-  up, down, left, right = 0, 1, 2, 3
-  
+class AccelListener(object):  
   def __init__(self):
-    self.prev_avgs = []
+    self.prev_avg = []
     self.curr_data = []
-    self.num_prevs = 1
     self.motion_threshold = 1.0
-    #self.movement_occurred = False
     self.ignore_up = False
     self.ignore_down = False
     self.ignore_left = False
@@ -25,53 +22,53 @@ class AccelListener(object):
     curr_avg = [0.0, 0.0]
     
     while True:
-      #time.sleep(INTERVAL)
+      # time.sleep(INTERVAL)
       raw_input("...")
       self.refresh_data()
       for datum_str in self.curr_data:
         datum = datum_str.split()
         curr_avg = []
         for i in range(len(curr_avg)):
-          curr_avg = [curr_avg[i] + (float)(datum[i]) for i in range(3)]
+          curr_avg = [curr_avg[i] + (float)(datum[i]) for i in range(2)]
       l = len(self.curr_data)
       curr_avg = [a / l for a in curr_avg]
-      #figure out which direction the phone moved
+      # figure out which direction the phone moved
       for i in range(len(curr_avg)):
-        if abs(curr_avg[i] - prev_avgs[i]) > motion_threshold:
-          #movement_occurred = True
-          if (curr_avg[i] - prev_avgs[i]) > motion_threshold:
-            if i==1:
-              #it moved in the right direction
-              if self.ignore_right == False:
+        if abs(curr_avg[i] - prev_avg[i]) > motion_threshold:
+          if (curr_avg[i] - prev_avg[i]) > motion_threshold:
+            if i == 0:
+              # it moved in the right direction
+              if not self.ignore_right:
                 self.ignore_left = True
-                self.register_direction(3)
+                self.register_direction(AccelListener.RIGHT)
               else:
                 self.ignore_right = False
             else:
-              #it moved in the up direction
-              if self.ignore_up == False:
+              # it moved in the up direction
+              if not self.ignore_up:
                 self.ignore_down = True
-                self.register_direction(0)
+                self.register_direction(AccelListener.DOWN)
               else:
                 self.ignore_up = False
-          elif (prev_avgs[i] - curr_avg[i]) > motion_threshold:
-            if i==1:
-              #it moved in the left direction
-              if self.ignore_left == False:
+          else:
+            if i == 0:
+              # it moved in the left direction
+              if not self.ignore_left:
                 self.ignore_right = True
-                self.register_direction(2)
+                self.register_direction(AccelListener.LEFT)
               else:
                 self.ignore_left = False
             else:
-              #it moved in the down direction
-              if self.ignore_down == False:
+              # it moved in the down direction
+              if not self.ignore_down:
                 self.ignore_up = True
-                self.register_direction(1)
+                self.register_direction(AccelListener.DOWN)
               else:
                 self.ignore_down = False
-      self.prev_avgs.append(curr_avg)
-      if len(self.prev_avgs) > self.num_prevs:
-        self.prev_avgs = self.prev_avgs[-2:]        
+      # self.prev_avg.append(curr_avg)
+      # if len(self.prev_avg) > self.num_prevs:
+      #   self.prev_avg = self.prev_avg[-2:]
+      self.prev_avg = curr_avg
 
   def register_direction(self, direction):
     with open("data/curr_direction.txt", "w+") as f:
