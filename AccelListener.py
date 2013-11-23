4d9f71f54112e66fbeb42d2b36565fa6991f8d47
Rohan <ronuchit@yahoo.com>
2013-11-23 13:55:32 -0800

    work for accellistener

--- AccelListener.py
+++ AccelListener.py
@@ -1,3 +1,5 @@
+import time
+
 # DATA_PER_INTERVAL = 5;
 INTERVAL = 0.1;
 
@@ -5,16 +7,34 @@ class AccelListener(object):
   up, down, left, right = 0, 1, 2, 3
   
   def __init__(self):
-    self.prev_samples = [];
+    self.prev_avgs = []
     self.curr_data = []
-    self.change_threshold = 1.0;
+    self.num_prevs = 1
+    self.motion_threshold = 1.0
     self.going_back = False
 
-  def refresh(self):
+  def refresh_data(self):
     with open("data/accel_data.txt", "r+") as f:
       self.curr_data = f.readlines()
 
   def listen(self):
-    while True:
-      
+    curr_avg = [0.0, 0.0, 0.0]
     
+    while True:
+      time.sleep(INTERVAL)
+      self.refresh_data()
+      for datum_str in self.curr_data:
+        datum = datum_str.split()
+        curr_avg = [curr_avg[i] + (float)datum[i] for i in range(3)]
+      l = len(self.curr_data)
+      curr_avg = [a / l for a in curr_avg]
+      
+      
+
+      self.prev_avgs.append(curr_avg)
+      if len(self.prev_avgs) > self.num_prevs:
+        self.prev_avgs = self.prev_avgs[-3:]        
+
+  def register_direction(self, direction):
+    with open("data/curr_direction.txt", "w+") as f:
+      f.write(direction)
