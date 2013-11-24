ce79d0c9771de5a8fe179ec9e9f92fe20c728e05
Rohan <ronuchit@yahoo.com>
2013-11-24 00:26:00 -0800

    adding more cmd line args

--- src/execute.sh
+++ src/execute.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 if [ $# -ne 5 ] ; then
-    echo "Usage: bash $0 [MAZE_X_SIZE] [MAZE_Y_SIZE] [MAZE_DIFFICULTY (0/1/2/3)] [PACE (0/1/2)] [INVINCIBILITY (0/1)]"
+    echo "Usage: bash $0 [MAZE_X_SIZE] [MAZE_Y_SIZE] [MAZE_DIFFICULTY (0/1/2/3)] [PACE (ms/update)] [INVINCIBILITY (0/1)]"
     exit
 fi
 
+echo $4 > data/game_info.txt
+echo $5 >> data/game_info.txt
 javac MazeGenerator.java
 java MazeGenerator $1 $2 $3
 python input_listener.py &
